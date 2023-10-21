317f6c8cd5303f90d66ef39b721c1e6119ea7256
Aldi Triavin <aldi3avin@gmail.com>
2023-10-21 23:20:25 +0700

    add utils

---
+++ src/main/java/com/juaracoding/utils/Constants.java
@@ -0,0 +1,15 @@
+package com.juaracoding.utils;
+
+public class Constants {
+    // Global Variable
+    public static final String CHROME = "chrome";
+    public static final String FIREFOX = "firefox";
+    public static final String URLHOMEPAGE = "https://shop.demoqa.com/";
+    public static final String URLMYACCOUNT = "https://shop.demoqa.com/my-account/";
+    public static final long TIMEOUT = 10;
+    public static final long SORTDELAY = 2;
+    public static final long LONGDELAY = 5;
+    public static final String USERNAME = "tesUser6801";
+    public static final String EMAIL = "aldi2@gmail.com";
+    public static final String PASSWORD = "altShift+F4!";
+}
