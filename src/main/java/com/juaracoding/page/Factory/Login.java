1d13b73dbe96d0e5a7b92a6b588c190b883b693f
Aldi Triavin <aldi3avin@gmail.com>
2023-10-22 12:02:47 +0700

    update add product,login,logout (page factory)

--- src/main/java/com/juaracoding/page/Factory/Login.java
+++ src/main/java/com/juaracoding/page/Factory/Login.java
@@ -1,6 +1,7 @@
 package com.juaracoding.page.Factory;
 
 import com.juaracoding.driver.DriverSingleton;
+import com.juaracoding.utils.LoginElement;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -9,64 +10,37 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Login {
     private WebDriver driver;
-    private JavascriptExecutor jse;
-    private int windowHeight;
 
-    @FindBy(xpath = "//input[@id='username']")
+    @FindBy(xpath = LoginElement.USERNAMEXPATH)
     private WebElement username;
-    @FindBy(xpath = "//input[@id='password']")
+    @FindBy(xpath = LoginElement.PASSWORDXPATH)
     private WebElement password;
-    @FindBy(xpath = "//button[@name='login']")
-    private WebElement regisBtn;
-    @FindBy(xpath = "//li[normalize-space()='The password field is empty.']")
-    private WebElement passRequired;
-    @FindBy(xpath = "//li[normalize-space()='Username is required.']")
-    private WebElement userRequired;
-    @FindBy(xpath = "//li[normalize-space()=': The username or password you entered is incorrect. Lost your password?']")
+    @FindBy(css = LoginElement.LOGINBTNXPATH)
+    private WebElement loginBtn;
+    @FindBy(xpath = LoginElement.INVALIDTXT)
     private WebElement txtInvalid;
-//    @FindBy(xpath = "//contains(text(),': The username or password you entered is incorrect. Lost your password?')")
-//    private WebElement txtInvalid;
-    //div[@id='primary']//li[1]
 
-
-
-
-    // constructor registration
+    // constructor Login
     public Login(){
         this.driver = DriverSingleton.getDriver();
-        this.jse = DriverSingleton.getJse();
-        this.windowHeight = DriverSingleton.getWindowHeight();
         PageFactory.initElements(driver, this);
     }
 
     // Login
     public void login(String username, String password){
+        DriverSingleton.findLoginForm();
         this.username.sendKeys(username);
         this.password.sendKeys(password);
-        regisBtn.click();
-    }
-
-    public void findForm(){
-        System.out.println(windowHeight);
-//        System.out.println(driver.manage().window().getSize());
-
-        if (windowHeight > 1382){
-            jse.executeScript("window.scrollBy(0,500)");
-            System.out.println("Scroll Down page by 500px");
-        } else {
-            jse.executeScript("window.scrollBy(0,600)");
-            System.out.println("Scroll Down page by 600px");
-        }
-
+        loginBtn.click();
     }
 
     public String getUserRequired(){
-        System.out.println(userRequired.getText());
-        return userRequired.getText();
+        System.out.println(txtInvalid.getText());
+        return txtInvalid.getText();
     }
     public String getPassRequired(){
-        System.out.println(passRequired.getText());
-        return passRequired.getText();
+        System.out.println(txtInvalid.getText());
+        return txtInvalid.getText();
     }
     public String getInvalid(){
         System.out.println(txtInvalid.getText());
--- src/main/java/com/juaracoding/page/Factory/Logout.java
+++ src/main/java/com/juaracoding/page/Factory/Logout.java
@@ -2,6 +2,7 @@ package com.juaracoding.page.Factory;
 
 import com.juaracoding.driver.DriverSingleton;
 import com.juaracoding.utils.Constants;
+import com.juaracoding.utils.LogoutElement;
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
@@ -10,23 +11,19 @@ import org.openqa.selenium.support.PageFactory;
 
 public class Logout {
     private WebDriver driver;
-    private JavascriptExecutor jse;
-    private int windowHeight;
 
-    @FindBy(xpath = "//a[contains(text(),'Log out')]")
+    @FindBy(xpath = LogoutElement.LOGOUTBTNXPATH)
     private WebElement logoutBtn;
 
 
     // constructor Logout
     public Logout(){
         this.driver = DriverSingleton.getDriver();
-        this.jse = DriverSingleton.getJse();
-        this.windowHeight = DriverSingleton.getWindowHeight();
         PageFactory.initElements(driver, this);
     }
 
     // Logout
-    public void login(String username, String password, String email){
+    public void logoutUser(){
         if (driver.getCurrentUrl().equals(Constants.URLMYACCOUNT)){
             logoutBtn.click();
         } else {
