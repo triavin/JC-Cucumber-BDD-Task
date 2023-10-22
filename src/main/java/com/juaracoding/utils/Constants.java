1d661737382a1ace3499e88799a6308f798ee469
Aldi Triavin <aldi3avin@gmail.com>
2023-10-22 12:03:38 +0700

    update Constants

--- src/main/java/com/juaracoding/utils/Constants.java
+++ src/main/java/com/juaracoding/utils/Constants.java
@@ -6,10 +6,17 @@ public class Constants {
     public static final String FIREFOX = "firefox";
     public static final String URLHOMEPAGE = "https://shop.demoqa.com/";
     public static final String URLMYACCOUNT = "https://shop.demoqa.com/my-account/";
+    public static final String URLCART = "https://shop.demoqa.com/cart/";
     public static final long TIMEOUT = 10;
     public static final long SORTDELAY = 2;
     public static final long LONGDELAY = 5;
     public static final String USERNAME = "tesUser6801";
     public static final String EMAIL = "aldi2@gmail.com";
     public static final String PASSWORD = "altShift+F4!";
+    public static final String FIRSTNAME = "ALDI";
+    public static final String LASTNAME = "TRIAVIN";
+    public static final String TOWN = "tangerang";
+    public static final String PROVINCE = "Banten";
+    public static final String POSTCODE = "1234";
+    public static final String PHONE = "0811223345";
 }
